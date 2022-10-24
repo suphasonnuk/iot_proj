d759493cfc353b2821ffb58295fbb9400ce728ae
suphasonnuk <suphasonnuk@gmail.com>
2022-10-25 00:25:03 +0700

    fix bugs

--- main.py
+++ main.py
@@ -4,11 +4,16 @@ from flask import Flask
 import datetime, time
 import pandas as pd
 import numpy as np
-from flask import url_for, redirect, render_template, Response , request , session
+from flask import url_for, redirect, render_template, Response , request , session , flash , get_flashed_messages
 import cv2
 import os
+import datetime
 
 app = Flask(__name__)
+
+
+app.secret_key = "abc"  
+current_time = datetime.datetime.now()
 
 camera = cv2.VideoCapture(0)
 camera.set(3,480)
@@ -16,103 +21,32 @@ camera.set(4,520)
 camera.set(10,1000)
 
 global capture,rec_frame, grey, switch, neg, face, rec, out
-capture=0
+
+
+data_use = None
+admin = "suphason"
+user_email = []
+user_name = []
+error = None
+user_password = []
+user_id = []
 grey=0
 neg=0
 face=0
 switch=1
 rec=0
 
-admin = ''
+data_recieved = pd.DataFrame({
+            "user_name" : user_name,
+            "user_id" : user_id,
+            "user_password" : user_password,
+            "user_email" : user_email
+        ,})
+
+
+
 _login = False
-user_email = ""
-user_name = ""
-user_id = ""
-user_year = 0
-time_expected = ""
 visiblility = ""
-
-net = cv2.dnn.readNetFromCaffe('./saved_model/deploy.prototxt.txt', './saved_model/res10_300x300_ssd_iter_140000.caffemodel')
-
-
-try:
-    os.mkdir('./shots')
-except OSError as error:
-    pass
-
-def detect_face(frame):
-    global net
-    (h, w) = frame.shape[:2]
-    blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 1.0,
-        (300, 300), (104.0, 177.0, 123.0))   
-    net.setInput(blob)
-    detections = net.forward()
-    confidence = detections[0, 0, 0, 2]
-
-    if confidence < 0.5:            
-            return frame           
-
-    box = detections[0, 0, 0, 3:7] * np.array([w, h, w, h])
-    (startX, startY, endX, endY) = box.astype("int")
-    try:
-        frame=frame[startY:endY, startX:endX]
-        (h, w) = frame.shape[:2]
-        r = 480 / float(h)
-        dim = ( int(w * r), 480)
-        frame=cv2.resize(frame,dim)
-    except Exception as e:
-        pass
-    return frame
-
-def generate_frames():
-    while True:
-            
-        ## read the camera frame
-        success,frame=camera.read()
-    
-        frame = cv2.flip(frame, 1)
-        if not success:
-            break
-        else:
-            ret,buffer=cv2.imencode('.jpg',frame)
-            frame=buffer.tobytes()
-
-        yield(b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
-def gen_frames():  # generate frame by frame from camera
-    global out, capture,rec_frame
-    while True:
-        success, frame = camera.read()
-        if success:
-            if(face):                
-                frame= detect_face(frame)
-            if(grey):
-                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            if(neg):
-                frame=cv2.bitwise_not(frame)    
-            if(capture):
-                capture=0
-                now = datetime.datetime.now()
-                p = os.path.sep.join(['shots', "{}_{}.png".format( user_id , str(now).replace(":",''))])
-                cv2.imwrite(p, frame)
-            
-            if(rec):
-                rec_frame=frame
-                frame= cv2.putText(cv2.flip(frame,1),"Recording...", (0,25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255),4)
-                frame=cv2.flip(frame,1)
-            
-                
-            try:
-                ret, buffer = cv2.imencode('.jpg', cv2.flip(frame,1))
-                frame = buffer.tobytes()
-                yield (b'--frame\r\n'
-                       b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-            except Exception as e:
-                pass
-                
-        else:
-            pass
 
 @app.route('/')
 def index():
@@ -120,19 +54,45 @@ def index():
 
 @app.route('/registering/')
 def registering():
-    return render_template("main_page/register.html")
+    return render_template("register.html")
 
 @app.route('/register/' , methods  = ['POST' , 'GET'])
 def register():
-    global user_id, user_name , time_expected , years , visiblility ,  user_email
+    global user_id, user_name , time_expected , user_password , visiblility ,  user_email , data_recieved
     if request.method == 'POST':
-        user_name = request.form.get('user_name')
-        user_id = request.form.get('user_id')
-        years = request.form.get('user_year')
-        time_expected = request.form.get('user_time')
-        user_email = request.form.get('user_email') 
-        visiblility = "hidden"
-    return render_template("main_page/register.html")
+
+        _data_recieved = pd.DataFrame({
+            "user_name" : [request.form.get('user_name')],
+            "user_id" : [request.form.get('user_id')],
+            "user_password" : [request.form.get('user_password')],
+            "user_email" : [request.form.get('user_email')]
+        })
+
+        if request.form.get('user_name') not in user_name:
+            user_name.append(_data_recieved['user_name'].values[0])
+            user_id.append(_data_recieved['user_name'].values[0])
+            user_password.append(_data_recieved['user_name'].values[0])
+            user_email.append(_data_recieved['user_name'].values[0])
+            visiblility = "hidden"
+
+            data_recieved = pd.concat([data_recieved , _data_recieved])
+            return render_template("regis_suc.html")
+        
+        
+        else: 
+            flash("This username is already used")
+            return render_template("register.html")
+        
+    if request.method == 'GET':
+        return render_template("register.html")
+
+    return render_template("register.html")
+
+@app.route('/logout/' , methods = ['POST' , 'GET'])
+def logout():
+    global _login
+    _login = 0
+    return redirect(url_for("login_page"))
 
 
 @app.route('/home/' , methods = ['POST' , 'GET'])
@@ -141,12 +101,12 @@ def home():
 
 @app.route('/main/' , methods = ['POST' , 'GET'])
 def main():
-    return render_template("main_page/index.html" , id_visible = "hidden")
+    return render_template("index.html" , id_visible = "hidden")
 
 @app.route('/history/' , methods = ['POST' , 'GET'])
 def history():
     if (_login):
-        return render_template("main_page/history.html")
+        return render_template(" history.html")
     else:
         return render_template("login_error.html")
 
@@ -154,37 +114,45 @@ def history():
 @app.route('/que/', methods = ['POST' , 'GET'])
 def que():
     if (_login):
-        if (admin):
-            return render_template("main_page/que.html")     
+        if (data_use[0][0] == admin):
+            return render_template("que.html")  
+        else:
+            return render_template("login_error.html")
     else:
         return render_template("login_error.html")
 
-
-@app.route('/video')
-def video():
-    return Response(generate_frames() , mimetype='multipart/x-mixed-replace; boundary=frame')
-
+    
 @app.route('/login/', methods=['GET', 'POST'])
 def login():
-    global user_id, user_name , time_expected , years , visiblility , _login
+    global user_id, user_name , user_password , years , visiblility , _login , error, data_use
     if request.method == 'POST':
-        user_name = request.form.get('user_name')
-        user_id = request.form.get('user_id')
-        years = request.form.get('years')
-        time_expected = request.form.get('user_time')
-        _login = True
         visiblility = "hidden"
         face_visibility = visiblility
 
-        return render_template("index.html" , name = user_name , id = user_id , year = years ,time = time_expected , visible = face_visibility , id_visible = "")
+        data_use = data_recieved[(data_recieved['user_name'] == request.form.get("user_name")) & ((data_recieved['user_password'] == request.form.get("user_password")))].values.tolist()
+        if ( data_use != []):
+            flash("log in successful")
+            _login = True
+            return render_template("Dashboard.html" , name = user_name , id = user_id , visible = face_visibility , id_visible = "" , personal = data_use[0])
+        else:
+
+            flash("Your user name is not in our DataBase, try again")
+            return render_template("login.html")
+
+    if request.method == "GET":
+            
+            return render_template("login.html")
+
+
 
 @app.route('/login_page/', methods = ['POST' , 'GET'])
 def login_page():
+    global user_id, user_name , user_password , years , visiblility , _login
     if (_login):
 
-        return render_template("main_page/Dashboard.html" , user_name = user_name , user_id = user_id)     
+        return render_template("Dashboard.html" , user_name = user_name , user_id = user_id , personal = data_use[0])     
     else:
-        return render_template("main_page/login.html")
+        return render_template("login.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run('0.0.0.0', port = 80 , debug=True)
